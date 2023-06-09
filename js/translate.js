d3e15d86ae50a70ced385ef8453c53aa4b09a413
James <james.brand@canterbury.ac.nz>
2023-06-09 10:20:43 +0200

    initiate GitHub repo

---
+++ js/translate.js
@@ -0,0 +1,13 @@
+function googleTranslateElementInit() {
+
+new google.translate.TranslateElement({
+
+pageLanguage: 'en'
+
+}, 'google_translate_element');
+
+}
+
+var link = document.getElementById(':1.container');
+link.style.display = 'none'; //or
+link.style.visibility = 'hidden';
