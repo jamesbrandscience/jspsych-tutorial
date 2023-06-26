e4ea1530f71935141d6bc9d25d7955913704107b
James <james.brand@canterbury.ac.nz>
2023-06-26 13:35:26 +0200

    update

---
+++ images/lexical_decision_stimuli_JSON.js
@@ -0,0 +1,102 @@
+var stimuli = [
+  {
+    "item": "liška",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "sýr",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "ovce",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "banán",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "židle",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "hrášek",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "srdce",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "dýně",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "okurka",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "hroch",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "lišxa",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "xýr",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "ovxe",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "bxnán",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "židlx",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "hxášek",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "srdcx",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "xýně",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "okurkx",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "hrxch",
+    "real_word": "no",
+    "correct_key": "h"
+  }
+];
