5a0575a4714067cd352959420a05c520740f4da9
jamesbrandscience <james.brand.ac@gmail.com>
2023-08-15 13:22:57 +0200

    update

---
+++ lexical_decision_stimuli_audio_JSON.js
@@ -0,0 +1,102 @@
+var stimuli = [
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "untitled.wav",
+    "real_word": "no",
+    "correct_key": "h"
+  }
+];
