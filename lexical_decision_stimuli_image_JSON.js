300e7df9784b44a6cb3ea62fae07564cbcfc0134
jamesbrandscience <james.brand.ac@gmail.com>
2023-08-16 09:46:07 +0200

    update

---
+++ lexical_decision_stimuli_image_JSON.js
@@ -0,0 +1,52 @@
+var stimuli = [
+  {
+    "item": "fox.png",
+    "eat": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "cheese.png",
+    "eat": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "sheep.png",
+    "eat": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "banana.png",
+    "eat": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "chair.png",
+    "eat": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "pea.png",
+    "eat": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "heart.png",
+    "eat": "no",
+    "correct_key": "h"
+  },
+  {
+    "item": "pumpkin.png",
+    "eat": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "cucumber.png",
+    "eat": "yes",
+    "correct_key": "d"
+  },
+  {
+    "item": "hippo.png",
+    "eat": "no",
+    "correct_key": "h"
+  }
+];
