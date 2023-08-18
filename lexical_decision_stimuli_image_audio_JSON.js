129851476e9769e1bcf76e2f74cdb106766b7a10
jamesbrandscience <james.brand.ac@gmail.com>
2023-08-18 18:18:48 +0200

    update

---
+++ lexical_decision_stimuli_image_audio_JSON.js
@@ -0,0 +1,82 @@
+var stimuli = [
+  {
+    "image_left": "fox.png",
+    "image_center": "hippo.png",
+    "image_right": "cucumber.png",
+    "audio": "fox.wav",
+    "real_word": "yes",
+    "correct_choice": 0
+  },
+  {
+    "image_left": "cheese.png",
+    "image_center": "fox.png",
+    "image_right": "hippo.png",
+    "audio": "cheesx.wav",
+    "real_word": "no",
+    "correct_choice": 0
+  },
+  {
+    "image_left": "sheep.png",
+    "image_center": "cheese.png",
+    "image_right": "fox.png",
+    "audio": "sheep.wav",
+    "real_word": "yes",
+    "correct_choice": 0
+  },
+  {
+    "image_left": "banana.png",
+    "image_center": "sheep.png",
+    "image_right": "cheese.png",
+    "audio": "banxna.wav",
+    "real_word": "no",
+    "correct_choice": 0
+  },
+  {
+    "image_left": "chair.png",
+    "image_center": "banana.png",
+    "image_right": "sheep.png",
+    "audio": "banana.wav",
+    "real_word": "yes",
+    "correct_choice": 1
+  },
+  {
+    "image_left": "pea.png",
+    "image_center": "chair.png",
+    "image_right": "banana.png",
+    "audio": "chxir.wav",
+    "real_word": "no",
+    "correct_choice": 1
+  },
+  {
+    "image_left": "heart.png",
+    "image_center": "pea.png",
+    "image_right": "chair.png",
+    "audio": "pea.wav",
+    "real_word": "yes",
+    "correct_choice": 1
+  },
+  {
+    "image_left": "pumpkin.png",
+    "image_center": "heart.png",
+    "image_right": "pea.png",
+    "audio": "pxa.wav",
+    "real_word": "no",
+    "correct_choice": 2
+  },
+  {
+    "image_left": "cucumber.png",
+    "image_center": "pumpkin.png",
+    "image_right": "heart.png",
+    "audio": "heart.wav",
+    "real_word": "yes",
+    "correct_choice": 2
+  },
+  {
+    "image_left": "hippo.png",
+    "image_center": "cucumber.png",
+    "image_right": "pumpkin.png",
+    "audio": "pumpkxn.wav",
+    "real_word": "no",
+    "correct_choice": 2
+  }
+];
